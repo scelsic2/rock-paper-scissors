1b5f95bbdf287d88207ce64428b09d1c04497f54
scelsic2 <scelsic2@gmail.com>
2023-06-06 17:38:11 -0400

    v1

---
+++ index.js
@@ -0,0 +1,147 @@
+const rpsInput = document.querySelector('.rps-input')
+const submitBtn = document.querySelector('.submit-btn')
+const responseDiv = document.querySelector('.response-div')
+const inputP = document.createElement('p')
+const scoreDiv = document.querySelector('.score-div')
+const winnerDiv = document.querySelector('.winner-div')
+
+let word = ''
+let response = ''
+
+let userScore = 0;
+let computerScore = 0;
+
+// computer random select
+function computersResponse (computerChoices) {
+    computerChoices = ['Rock', 'Paper', 'Scissors']
+    const randomI = Math.floor(Math.random() * computerChoices.length)
+    response = computerChoices[randomI]
+    console.log('computer response: ' + response)
+    const computerP = document.createElement('p')
+    computerP.innerText = `Computer's Choice: ` + response
+    responseDiv.appendChild(computerP)
+    return response
+}
+
+// must recognize r, p, s
+function userInput (letter) {
+    letter = rpsInput.value
+    const caps = letter.toUpperCase()
+    
+    console.log('user choice: ' + caps)
+
+    switch (caps) {
+        case 'R':
+            console.log('valid entry')
+            word = 'Rock'
+            break;
+        case 'P':
+            console.log('valid entry')
+            word = 'Paper'
+            break;
+        case 'S':
+            console.log('valid entry')
+            word = 'Scissors'
+            break;
+        default:
+            console.log('invalid entry')
+            letter = null
+            console.log(letter)
+    }
+
+    if (letter != null) {
+        computersResponse()
+    } else {
+        word = ''
+        inputP.innerText = ''
+        responseDiv.appendChild(inputP)
+    }
+
+    return word
+}
+
+function printUserInput(input) {
+    input = userInput()
+    console.log('print user input: ' + input)
+
+    if (input === ''){
+        inputP.innerText = 'Invalid entry. Please try again.'
+        responseDiv.appendChild(inputP)
+    } else {
+      inputP.innerText = 'My Choice: ' + input
+    responseDiv.appendChild(inputP)  
+    }
+    
+    return input
+}
+
+function scoreKeeper (user, computer) {
+    winnerDiv.innerHTML = ''
+    scoreDiv.innerHTML = ''
+
+    let winner = ''
+    
+    user = word
+    computer = response
+    if (user === '') {
+        console.log('error')
+    } else if (user === computer) {
+        userScore = userScore + 1;
+        computerScore = computerScore + 1
+        winner = `It's a tie!`
+    } else if (user === 'Rock' && computer === 'Paper'){
+        computerScore = computerScore + 1
+        winner = 'Computer'
+    } else if (user === 'Rock' && computer === 'Scissors'){
+        userScore = userScore + 1;
+        winner = 'I'
+    } else if (user === 'Paper' && computer === 'Rock'){
+        userScore = userScore + 1;
+        winner = 'I'
+    } else if (user === 'Paper' && computer === 'Scissors'){
+        computerScore = computerScore + 1
+        winner = 'Computer'
+    } else if (user === 'Scissors' && computer === 'Rock'){
+        computerScore = computerScore + 1
+        winner = 'Computer'
+    } else if (user === 'Scissors' && computer === 'Paper'){
+        userScore = userScore + 1;
+        winner = 'I'
+    }
+
+    const printWinner = document.createElement('p')
+
+    if (winner === `It's a tie!`) {
+        printWinner.innerHTML = winner
+    } else if (winner === 'Computer') {
+        printWinner.innerHTML = winner + ' wins!'
+    } else if (winner === 'I') {
+        printWinner.innerHTML = winner + ' win!'
+    }
+
+    winnerDiv.appendChild(printWinner)
+
+    const printUserScore = document.createElement('p')
+    printUserScore.innerHTML = 'My Score: ' + userScore
+    scoreDiv.appendChild(printUserScore)
+
+    const printComputerScore = document.createElement('p')
+    printComputerScore.innerHTML = `Computer's Score: ` + computerScore
+    scoreDiv.appendChild(printComputerScore)
+
+}
+
+function go (e) {
+    e.preventDefault()
+    responseDiv.innerHTML = ''
+    printUserInput()
+    rpsInput.value = ''
+    scoreKeeper()
+}
+
+submitBtn.addEventListener('click', go)
+
+
+
+
+// total wins, losses, and ties in local storage
